925eaa9328b6df3f8157f66d9cdf817bab2037b1
Hernando <hjrcastro@outlook.es>
2023-03-19 20:17:37 -0500

    Correción validación de campos usuarios

--- middlewares/validaciones.js
+++ middlewares/validaciones.js
@@ -5,7 +5,7 @@ const { validationResult } = require('express-validator');
 const validarCampos = (req = request, res = response ,next) => {
     //Middleware para validar los campos e imprime el mensaje de error
     const errors = validationResult(req) ;
-    if(errors.isEmpty){
+    if(!errors.isEmpty){
         return res.status(400).json(errors);
     }
     next();
